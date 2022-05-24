ebe3e5e475cce6f3cec900d45944724888e8bfc1
Jon_Ogden <jogden1863@gmail.com>
2022-05-24 16:00:04 -0500

    idk

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import './App.css';
 
+import React, { useState } from "react";
+let array = ['X','O','','','X','','O','','']
+function updateX(pos,arr){
+    arr.splice(pos,1,'X')
+}
+
+function updateO(pos,arr){
+    arr.splice(pos,1,'O')
+}
+
 function App() {
   return (
     <div>
       <h1>Tic Tac Toe</h1>
       <div className='container'>
-      <button className='item'></button>
-      <button className='item'></button>
-      <button className='item'></button>
-      <button className='item'></button>
-      <button className='item'></button>
-      <button className='item'></button>
-      <button className='item'></button>
-      <button className='item'></button>
-      <button className='item'></button>
+      <button className='item'>{array[0]}</button>
+      <button className='item'>{array[1]}</button>
+      <button className='item'>{array[2]}</button>
+      <button className='item'>{array[3]}</button>
+      <button className='item'>{array[4]}</button>
+      <button className='item'>{array[5]}</button>
+      <button className='item'>{array[6]}</button>
+      <button className='item'>{array[7]}</button>
+      <button className='item'>{array[8]}</button>
       </div>
+      <h3>Turn:</h3>
     </div>
   );
 }
