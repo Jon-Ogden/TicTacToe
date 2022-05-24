8e4302a673683d16f6a4eed4c815c5ccd4c087b6
Jon_Ogden <jogden1863@gmail.com>
2022-05-24 16:58:12 -0500

    game works

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
 import './App.css';
-
 import React, { useState } from "react";
-let array = ['X','O','','','X','','O','','']
-function updateX(pos,arr){
-    arr.splice(pos,1,'X')
-}
+import Grid from './array'
 
-function updateO(pos,arr){
-    arr.splice(pos,1,'O')
-}
+let array = ['','','','','','','','','']
 
 function App() {
-  return (
-    <div>
-      <h1>Tic Tac Toe</h1>
-      <div className='container'>
-      <button className='item'>{array[0]}</button>
-      <button className='item'>{array[1]}</button>
-      <button className='item'>{array[2]}</button>
-      <button className='item'>{array[3]}</button>
-      <button className='item'>{array[4]}</button>
-      <button className='item'>{array[5]}</button>
-      <button className='item'>{array[6]}</button>
-      <button className='item'>{array[7]}</button>
-      <button className='item'>{array[8]}</button>
-      </div>
-      <h3>Turn:</h3>
-    </div>
-  );
-}
+   return(
+     <Grid
+        arr={array}
+     />
+   )
+};
 
 export default App;
--- src/array.js
+++ src/array.js
@@ -8,13 +8,47 @@ function updateO(pos,arr){
     arr.splice(pos,1,'O')
 }
 
-let [turn, setTurn] = useState('X')
-function changeTurn(){
-    if(turn == 'X'){
-        setTurn('O')
-    } else if(turn == 'O'){
-        setTurn('X')
+
+
+
+
+const Grid = ({arr})=>{
+    let [turn, setTurn] = useState('X')
+    function changeTurn(){
+        if(turn === 'X'){
+            setTurn('O')
+        } else if(turn === 'O'){
+            setTurn('X')
+        };
     };
+    function myTurn(value, grid,arr){
+        if(value == ''){
+            if(turn == 'X'){
+                updateX(grid,arr)
+                changeTurn()
+            } else if(turn == 'O'){
+                updateO(grid, arr)
+                changeTurn()
+            }
+        } 
+    }
+    return (
+        <div>
+          <h1>Tic Tac Toe</h1>
+          <div className='container'>
+          <button className='item'onClick={()=>{myTurn(arr[0], 0,arr)}}>{arr[0]}</button>
+          <button className='item'onClick={()=>{myTurn(arr[1], 1,arr)}}>{arr[1]}</button>
+          <button className='item'onClick={()=>{myTurn(arr[2], 2,arr)}}>{arr[2]}</button>
+          <button className='item'onClick={()=>{myTurn(arr[3], 3,arr)}}>{arr[3]}</button>
+          <button className='item'onClick={()=>{myTurn(arr[4], 4,arr)}}>{arr[4]}</button>
+          <button className='item'onClick={()=>{myTurn(arr[5], 5,arr)}}>{arr[5]}</button>
+          <button className='item'onClick={()=>{myTurn(arr[6], 6,arr)}}>{arr[6]}</button>
+          <button className='item'onClick={()=>{myTurn(arr[7], 7,arr)}}>{arr[7]}</button>
+          <button className='item'onClick={()=>{myTurn(arr[8], 8,arr)}}>{arr[8]}</button>
+          </div>
+          <h3>Turn: {turn}</h3>
+        </div>
+      );
 };
 
-
+export default Grid;
